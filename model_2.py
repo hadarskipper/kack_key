7d91062198e4e8d0e969cb26d506901995a5b99b
unknown <hadarskipper@gmail.com>
2019-08-07 13:46:20 +0300

    add models example

---
+++ model_2.py
@@ -0,0 +1,35 @@
+import numpy as np
+import time
+
+class Model2Runner:
+    def __init__(self, out_dic, output_key, input_key):
+        self.t_predict = 10
+        self.t_overlap = 5
+        self.n_laps_predict = 5
+        self.out_dic = out_dic
+        self.output_key = output_key
+        self.input_key = input_key
+
+
+    def run(self):
+        proba_list = [np.zeros((3,)) for _ in range(self.n_laps_predict)]
+        curr_time = time.time()
+        while True:
+            data_dic, CAS_fs, sampwidth = get_last_T_data(self.t_predict)
+            data = data_dic[self.input_key]
+            proba = predict_proba(x_arr)
+            proba_list.pop(0)
+            proba_list.append(proba)
+            self.out_dic[self.output_key] = agg_time(proba_list)
+            curr_time += self.t_overlap
+            try:
+                time.sleep(curr_time - time.time())
+            except:
+                pass
+
+def predict_proba(x_arr):
+    time.sleep(0.1)
+    return np.random.random((2,))
+
+def agg_time(proba_list):
+    return np.argmax(np.mean(np.stack(proba_list, axis=0), axis=0))
