f2876f0f4e6fffb3ba581a3a0d05674d8b084333
unknown <hadarskipper@gmail.com>
2019-08-07 20:19:26 +0300

    cleaned

--- Server.py
+++ Server.py
@@ -3,15 +3,12 @@ from threading import Thread
 import multiprocessing as mp
 import numpy
 import time
-import soundfile
 import model_1, model_2
-import db_lib
-import math
-import sqlalchemy
-import struct
+# import db_lib
 import wave
 import os
 
+mp.freeze_support()
 
 # Constants
 file_min_max   = 5  # Maximum length (Minutes) for a record
@@ -29,11 +26,13 @@ server_input_log = []
 exit_bool        = [False]
 current_prediction = {}
 channel_queues     = []
+channel_process    = []
+model_process = [[] for _ in range(5)]
 
 # Paths
 PATH_RECORDING = r'C:\PS\{}\{}\{}.wav'
 manager = mp.Manager()
-model_output = manager.dict()
+model_output_list = [manager.dict() for _ in range(5)]
 
 model_list = [model_1.Model1Runner, model_2.Model2Runner]
 
@@ -46,25 +45,10 @@ def get_meta_message(udp_raw):
     return ret
 
 
-def server_input():
-    while 1:
-        if exit_bool[0]:
-            print('in server_input_thread - exit_bool is True...')
-            break
-        s = input(' * q - exit \nenter new message for server: ')
-        server_input_log.append(s)
-
-        if server_input_log[-1] == 'q':
-            exit_bool[0] = True
-            serversocket.close()
-        else:
-            pass
-    print('server_input_thread has finished...')
-
 # Send NumPy array, every channel at a time
-def RunModel():
-    for i in range(NUM_CHANNELS):
-        input_to_model_q.put(numpy.asarray(live_data_list[i]))
+# def RunModel():
+#     for i in range(NUM_CHANNELS):
+#         input_to_model_q.put(numpy.asarray(live_data_list[i]))
 
 def data_feed(print_droped=True):
     while 1:
@@ -95,7 +79,7 @@ def writeChannel(q, arr, index):
                     q.get()
                     break
 
-                wave_file = wave.open(os.path.join(dir_path, f'{file_num}.wav'), "wb")
+                wave_file = wave.open(os.path.join(dir_path, '{}.wav'.format(file_num)), "wb")
                 wave_file.setparams((1, 2, CAS_fs, 0, 'NONE', 'not compressed'))
                 curr_time = time.time()
                 for i in range(file_min_max):
@@ -107,28 +91,34 @@ def writeChannel(q, arr, index):
 
 
 
-def recordChannel(channel):
-    pass
-
-
 def main():
 
     # Allocate threads
     data_feed_thread = Thread(target=data_feed, args=(False,))
     data_feed_thread.start()
 
-    server_input_thread = Thread(target=server_input)
-    server_input_thread.start()
 
     for i in range(5):
         channel_queues.append(mp.Queue())
-        mp.Process(target=writeChannel, args=(channel_queues[i], live_data_list[i], current_idx_list[i]))
+        channel_process.append(mp.Process(target=writeChannel, args=(channel_queues[i], live_data_list[i], current_idx_list[i])))
+        channel_process[i].start()
+
+    for i in range(5):
+        for model in model_list:
+            model_process[i].append(mp.Process(model.run, args=(live_data_list[i], current_idx_list[i], model_output_list[i])))
+            model_process[i][-1].start()
+
+
+    curr_time = time.time()
+    while True:
+        if time.time()>curr_time:
+            curr_time += 5
+            print(model_output_list[0])
 
     data_feed_thread.join()
 
 
 if __name__ == '__main__':
-
     #_ Listen to the stream of data
     data_feed_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     data_feed_socket.bind((data_feed_HOST, data_feed_PORT))
